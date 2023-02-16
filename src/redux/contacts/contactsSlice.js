8b4a9478299c40560479736e305fb787e5d2aae3
Dr-Artem <artic12101997dr@gmail.com>
2023-02-16 02:50:35 +0200

    added reducer

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,19 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { fetchContacts, addContact, deleteContact } from './contactsOperations';
-
-const onPending = state => {
-    state.isLoading = true;
-    state.error = null;
-};
-
-const onRejected = (state, action) => {
-    state.isLoading = false;
-    state.error = action.payload;
-};
-
-const onFinally = state => {
-    state.error = null;
-};
 
 export const contactsSlice = createSlice({
     name: 'contacts',
@@ -22,34 +7,4 @@ export const contactsSlice = createSlice({
         isLoading: false,
         error: null,
     },
-    extraReducers: {
-        [fetchContacts.pending]: onPending,
-        [fetchContacts.fulfilled](state, action) {
-            state.isLoading = false;
-            state.error = null;
-            state.items = action.payload;
-        },
-        [fetchContacts.rejected]: onRejected,
-
-        [addContact.pending]: onPending,
-        [addContact.fulfilled](state, action) {
-            state.isLoading = false;
-            state.error = null;
-            state = state.items.push(action.payload);
-        },
-        [addContact.rejected]: onRejected,
-
-        [deleteContact.pending]: onPending,
-        [deleteContact.fulfilled](state, action) {
-            state.isLoading = false;
-            state.error = null;
-            state.items = state.items.filter(contact => {
-                return contact.id !== action.payload.id;
-            });
-        },
-        [deleteContact.rejected]: onRejected,
-        [deleteContact.finally]: onFinally,
-    },
 });
-
-export const contactsReducer = contactsSlice.reducer;
